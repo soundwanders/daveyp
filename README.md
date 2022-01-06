ff40fb66f668b45a863072ae5b0fbdf90e3e8f65
soundwanders <soundwanders@gmail.com>
2022-01-05 22:50:12 -0500

    Created ultranightmare album data file, need to map json array and render in UI

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-## soundwanders
-
-## Questions for davey
-
-- would you like a nav bar on desktop or just the icon to toggle it on mobile?
-
-- if yes to nav bar, stay fixed at top of page or to move off screen like normal content as you scroll down page?
+#### soundwanders
--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-// dperron website
-// connect to spotify api to get music data, create loop to append albums to grid
-// click album to bring up spotify embedded page
+// you need to create a new div for each album and append it to the spotify row div
+
+let dataExport;
+
+function grabData(returnData) {
+    fetch("./spotify.json")
+    .then(response => {
+        return response.json().then(function(data) {
+            returnData = JSON.parse(data);
+            {data.map(({ id, name, url }) => (
+                <iframe
+                  key={id}
+                  src={url}
+                  width="300" 
+                  height="380" 
+                  frameborder="0" 
+                  allowtransparency="true" 
+                  allow="encrypted-media"
+                  aria-label={`Listen to album ${name}`}
+                >
+                </iframe>
+            ))};
+            console.log(returnData);
+        });
+    })
+}
+
+grabData(dataExport);
