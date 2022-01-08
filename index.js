f311040b1034adb9bc4148b25f6fe32e58d51c8a
soundwanders <soundwanders@gmail.com>
2022-01-07 20:07:30 -0500

    Spotify embed albums, need dynamic render from js loop but good placeholder for now

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
-// you need to create a new div for each album and append it to the spotify row div
-
-let dataExport;
-
-function grabData(returnData) {
-    fetch("./spotify.json")
-    .then(response => {
-        return response.json().then(function(data) {
-            returnData = JSON.parse(data);
-            {data.map(({ id, name, url }) => (
-                <iframe
-                  key={id}
-                  src={url}
-                  width="300" 
-                  height="380" 
-                  frameborder="0" 
-                  allowtransparency="true" 
-                  allow="encrypted-media"
-                  aria-label={`Listen to album ${name}`}
-                >
-                </iframe>
-            ))};
-            console.log(returnData);
-        });
-    })
-}
-
-grabData(dataExport);
+// // {albums.map(({ id, name, url }) => (
+// //     <iframe
+// //       key={id}
+// //       src={url}
+// //       width="300" 
+// //       height="380" 
+// //       frameborder="0" 
+// //       allowtransparency="true" 
+// //       allow="encrypted-media"
+// //       aria-label={`Listen to album ${name}`}
+// //     >
+// //     </iframe>
+// // ))}
+// 
+// const readJson = () => {
+//     fetch('./spotify.json')
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error("HTTP error " + response.status);
+//         }
+//         return response.json();
+//     })
+//     .then(json => {
+//         this.albums = json;
+//         console.log(this.albums);
+// 
+//         for (i=0; i < albums.length; i++) {
+//             return (
+//                 <iframe
+//                 key={id}
+//                 src={url}
+//                 width="300" 
+//                 height="380" 
+//                 frameborder="0" 
+//                 allowtransparency="true" 
+//                 allow="encrypted-media"
+//                 aria-label={`Listen to album ${name}`}
+//                 >
+//                 </iframe>
+//             )
+//         }
+//     })
+//     .catch(function () {
+//         this.dataError = true;
+//     })
+// };
+// 
+// readJson();
