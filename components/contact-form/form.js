49d088610a832377d698632fdd9b5c9ad1713319
soundwanders <soundwanders@gmail.com>
2022-05-09 18:25:41 -0400

    Contact form validation fixed. Background image improvement to take up full viewport

--- components/contact-form/form.js
+++ components/contact-form/form.js
@@ -1,76 +1,74 @@
-// jquery for contact form validation
 
 (function ($) {
-  'use strict';
+  "use strict";
 
-  /** Validate after type */
-  $('.validate-input .input').each(function () {
-    $(this).on('blur', function () {
-      if (validate(this) == false) {
+  /* Validate after type */
+  $('.validate-input .input').each(function() {
+    $(this).on('blur', function() {
+      if(validate(this) == false) {
         showValidate(this);
-      } else {
+      }
+      else {
         $(this).parent().addClass('true-validate');
       }
-    });
-  });
+    })    
+  })
 
-  /** Validate form input */
-  let input = $('.validate-input .input');
 
-  $('.validate-form').on('submit', function () {
-    let check = true;
+  /* Validate data before submission is accepted */
+  const input = $('.validate-input .input');
 
-    for (let i = 0; i < input.length; i++) {
-      if (validate(input[i]) == false) {
+  $('.validate-form').on('submit',function() {
+    const check = true;
+
+    for(const i=0; i<input.length; i++) {
+      if(validate(input[i]) == false) {
         showValidate(input[i]);
-        check = false;
+        check=false;
       }
     }
+
     return check;
   });
 
-  $('.validate-form .input').each(function () {
-    $(this).focus(function () {
+
+  $('.validate-form .input').each(function() {
+    $(this).focus(function(){
       hideValidate(this);
       $(this).parent().removeClass('true-validate');
     });
   });
 
-  function validate(input) {
-    if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-      if (
-        $(input)
-          .val()
-          .trim()
-          .match(
-            /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
-          ) == null
-      ) {
+  function validate (input) {
+    if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+      if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
         return false;
       }
-    } else {
-      if ($(input).val().trim() == '') {
+    }
+    else {
+      if($(input).val().trim() == '') {
         return false;
       }
     }
   }
 
   function showValidate(input) {
-    let thisAlert = $(input).parent();
-    $(thisAlert).addClass('alert-validate');
-    $(thisAlert).append('<span class="btn-hide-validate">&#10006;</span>');
+    const thisAlert = $(input).parent();
 
-    $('.btn-hide-validate').each(function () {
-      $(this).on('click', function () {
-        hideValidate(this);
-        $('textarea[name="message"]').removeAttr('placeholder');
+    $(thisAlert).addClass('alert-validate');
+
+    $(thisAlert).append('<span class="btn-hide-validate">&#x2613;</span>')
+    $('.btn-hide-validate').each(function() {
+      $(this).on('click',function() {
+          hideValidate(this);
       });
     });
   }
 
   function hideValidate(input) {
-    let thisAlert = $(input).parent();
+    const thisAlert = $(input).parent();
     $(thisAlert).removeClass('alert-validate');
     $(thisAlert).find('.btn-hide-validate').remove();
   }
+  
 })(jQuery);
