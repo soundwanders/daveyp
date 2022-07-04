4e5c6568cf5ad5630d700990e5ab27c6fd71db91
soundwanders <soundwanders@gmail.com>
2022-07-04 15:53:20 -0400

    Add service worker for cacheing, fix hero layout issue, delete unused fonts:

--- HeroHeight.js
+++
@@ -1,9 +0,0 @@
-function resizeHero() {
-  var $headerWrapper = $('#header-wrapper');
-  $headerWrapper.css('height', '' + $(window).height());
-}
-
-$(document).ready(function() {
-  if($(window).width() > 1080) resizeHero();
-  $(window).bind('resize', resizeHero);
-})(jQuery);
