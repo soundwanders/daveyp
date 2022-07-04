9fa13d596817923d849eca0228f1f6c234384062
soundwanders <soundwanders@gmail.com>
2022-07-03 23:02:29 -0400

    test buffer

--- HeroHeight.js
+++ HeroHeight.js
@@ -1,6 +1,6 @@
 function resizeHero() {
   var $headerWrapper = $('#header-wrapper');
-  $headerWrapper.css('height', '' + $(window).height());
+  $headerWrapper.css('height', '' + $(window).height() - 40);
 }
 
 $(document).ready(function() {
