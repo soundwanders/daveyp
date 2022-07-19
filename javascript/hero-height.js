d47ecb1bee28ddd65ac460d3fa04c30b87cb0437
soundwanders <soundwanders@gmail.com>
2022-07-18 20:42:30 -0400

    Fix mobile portrait height, set intro section to trigger fade in animation without need to scroll down

--- javascript/hero-height.js
+++ javascript/hero-height.js
@@ -1,10 +1,11 @@
+// Dynamically resize Hero section to 100% of the viewport height on all devices except mobile
 function resizeHero() {
   var $headerWrapper = $('#header-wrapper');
   $headerWrapper.css('height', '' + $(window).height());
 };
 
 $(document).ready(function () {
-  if ($(window).width() > 1020) {
+  if ($(window).width() > 980) {
     resizeHero();
     $(window).bind('resize', resizeHero);
   }
