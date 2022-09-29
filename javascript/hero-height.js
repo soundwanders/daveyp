5737530accbbbbb1fa491fedd4ee62b43b3879af
soundwanders <soundwanders@gmail.com>
2022-09-28 22:13:16 -0400

    Update source code

--- javascript/hero-height.js
+++ javascript/hero-height.js
@@ -1,11 +1,10 @@
-// Dynamically resize Hero section to 100% of the viewport height on all devices except mobile
 function resizeHero() {
   var $headerWrapper = $('#header-wrapper');
   $headerWrapper.css('height', '' + $(window).height());
 };
 
 $(document).ready(function () {
-  if ($(window).width() > 980) {
+  if ($(window).width() > 1020) {
     resizeHero();
     $(window).bind('resize', resizeHero);
   }
